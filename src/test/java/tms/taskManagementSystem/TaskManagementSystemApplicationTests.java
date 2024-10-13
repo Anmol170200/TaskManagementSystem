2cc9a87beeaf30c8472edc0c72e7b881b9004f54
Anmol Tomar <anmoltomar170200@gmail.com>
2024-10-13 18:11:42 +0530

    hello world

---
+++ src/test/java/tms/taskManagementSystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tms.taskManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
